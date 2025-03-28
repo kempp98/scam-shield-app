ae11b5d234265f7051b1592592976603c5e6110d
Parker Kemp <kempparker98@gmail.com>
2025-03-27 20:05:40 -0500

    fix deployment error

--- src/components/educational/module-list.tsx
+++ src/components/educational/module-list.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
